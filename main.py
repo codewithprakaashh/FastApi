a29a6d1fb277689a9fe119577d796698009a15df
codewithprakaashh <prakashgyawali3825@gmail.com>
2024-03-05 14:51:31 +0530

    removing the startup thing which i did earlier coz it is of no use

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import asyncio
-from asyncio import create_task
+
 from fastapi import FastAPI
 import models
 from database import engine
@@ -16,23 +15,10 @@ models.Base.metadata.create_all(bind=engine)
 app.mount('/static',StaticFiles(directory='static'), name='static')
 
 
-
-
 @app.get('/')
 async def root():
     return RedirectResponse(url="/todos",status_code = status.HTTP_302_FOUND)
 
-
-async def keep_alive_task():
-  # Simulate keeping the server alive (replace with your actual logic)
-  print("Background Task: Keeping server alive...")
-  await asyncio.sleep(600)  
-
-async def startup():
-  # Schedule background task here
-  task = create_task(keep_alive_task())
-
-app.add_event_handler("startup", startup)
 
 app.include_router(auth.router)
 app.include_router(todos.router)

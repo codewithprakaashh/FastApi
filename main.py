e2870fb7a23a92d4a1fa158fea68f341a03768b1
codewithprakaashh <prakashgyawali3825@gmail.com>
2024-03-05 14:41:04 +0530

    created a startup event for preventing render to stop

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import asyncio
+from asyncio import create_task
 from fastapi import FastAPI
 import models
 from database import engine
@@ -13,10 +15,29 @@ models.Base.metadata.create_all(bind=engine)
 
 app.mount('/static',StaticFiles(directory='static'), name='static')
 
+
+
+
 @app.get('/')
 async def root():
     return RedirectResponse(url="/todos",status_code = status.HTTP_302_FOUND)
 
+
+async def keep_alive_task():
+  # Simulate keeping the server alive (replace with your actual logic)
+  print("Background Task: Keeping server alive...")
+  await asyncio.sleep(600)  
+
+async def startup():
+  # Schedule background task here
+  task = create_task(keep_alive_task())
+
+app.add_event_handler("startup", startup)
+
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(users.router)
+
+
+
+
